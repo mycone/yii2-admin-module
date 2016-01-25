7dc2fd31b1de8121a60126838999b76d5d1a81be
Sergei Gulin <gulin-sergei+github@ya.ru>
2016-01-25 16:53:53 +0300

    Update Input.php

--- forms/widgets/Input.php
+++ forms/widgets/Input.php
@@ -22,11 +22,11 @@ class Input extends Base
     /**
      * @inheritdoc
      */
-    public function renderInput($value, $attribute = null)
-    {
-        return Html::activeInput($this->type, $this->model, $attribute ? $attribute : $this->attribute, [
-            'class' => 'form-control',
-            'value' => $value,
-        ]);
+    public function renderInput($value, $attribute = null) {
+        Html::addCssClass($this->options, 'form-control');
+        if (!isset($this->options['value'])) {
+            $this->options['value'] = $value;
+        }
+        return Html::activeInput($this->type, $this->model, $attribute ? $attribute : $this->attribute, $this->options);
     }
 }
