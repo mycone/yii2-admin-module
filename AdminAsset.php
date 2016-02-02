14c2ea9eb09fb86a8bb8d9c630ce93c8b8c1c73b
Sergei Gulin <s.gulin@rootmedia.ru>
2016-02-02 16:00:54 +0300

    fix admin asset

--- AdminAsset.php
+++ AdminAsset.php
@@ -1,32 +1,22 @@
 <?php
-/**
- * @link http://www.yiiframework.com/
- * @copyright Copyright (c) 2008 Yii Software LLC
- * @license http://www.yiiframework.com/license/
- */
 
 namespace asdfstudio\admin;
 
 use yii\web\AssetBundle;
 
-/**
- * @author Qiang Xue <qiang.xue@gmail.com>
- * @since 2.0
- */
-class AdminAsset extends AssetBundle
-{
+class AdminAsset extends AssetBundle {
     public $sourcePath = '@vendor/sgdot/yii2-admin-module/assets';
-    public $css = [
+    public $css        = [
         'css/sb-admin.css',
     ];
-    public $js = [
+    public $js         = [
         'js/admin.js',
         'js/form.js',
     ];
-    public $depends = [
+    public $depends    = [
         'yii\web\YiiAsset',
         'yii\bootstrap\BootstrapAsset',
         'rmrevin\yii\fontawesome\AssetBundle',
-	    'cakebake\bootstrap\select\BootstrapSelectAsset',
+        'cakebake\bootstrap\select\BootstrapSelectAsset',
     ];
 }
